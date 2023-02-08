e97769d7a1b48bbe620eb562aee2ddcc67d93a34
soyun <soyoon4122@naver.com>
2023-02-08 19:38:00 +0900

    Feat: add location search api

--- src/api/api.js
+++ src/api/api.js
@@ -15,12 +15,22 @@ import { instance } from './axios';
 //   }
 // };
 
+// 키워드
 export const getKeyword = async (page, keyword) => {
   const sending = {
     pageno: page,
     keyword: keyword
   };
   return instance.get(`tourlist/keyword`, { params: { ...sending } });
+};
+
+// 지역
+export const getLocation = async (page, location) => {
+  const sending = {
+    pageno: page,
+    areacode: location
+  };
+  return instance.get(`tourlist/location`, { params: { ...sending } });
 };
 
 export const getDetails = async (detailId) => {
